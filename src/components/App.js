9e04449e8e1c02ae981c47b3965a70c0c8ccff07
Mahmoud jamal <mahmoud1234goraba@gmail.com>
2021-12-09 14:27:33 +0200

    upload files

---
+++ src/components/App.js
@@ -0,0 +1,87 @@
+
+import React from 'react';
+
+function App(){
+    return(
+      <div>
+<div id="div1"></div>
+ 
+
+
+<div class="p1">
+    <p >
+         works: Web designer , flutter developer</p>  
+        <p >Address :Ahmoon City - Egypt - Elmounofia</p>   
+       <p >Email:mahm4goraba@gmail.com</p>  
+    
+    <p >phone: 01098836658</p>
+    
+    <h1>Skills</h1>
+    <p >java development :</p>
+        <progress  value="75" max="100"> 75% </progress>
+      <p >Flutter&amp;Dart :</p>
+        <progress  value="70" max="100"> 70% </progress>
+        <p >Web design :</p>
+        <progress  value="90" max="100"> 90% </progress>
+            <p>video editing :</p>
+        <progress  value="75" max="100"> 75% </progress>
+        <p /> 
+                    <p >Arabic:</p>
+        <progress  value="95" max="100">95% </progress>
+                <p >English :</p>
+        <progress  value="75" max="100"> 75% </progress>
+                <p >French :</p>
+        <progress  value="55" max="100"> 55% </progress>
+        
+        <div id="topright">
+        <h1>WORKEXPERIANCE</h1>
+    <pre > - Works as front end developer in <br/>
+freelancer website from 2019
+</pre>
+  <hr />
+    <pre >- AS software Engineer : <br/>
+Designs and creates software solutions to solve <br/>
+pain points for variousclients <br/>
+Checks feasibility ofsoftware prototypes <br/>
+Modifies code to fix errors</pre>   
+      <hr />
+    <pre >- As software developer : <br/>
+Developed and implemented software solutions <br/>
+based on client requirements <br/>
+Tested and refined software prior to rollout <br/>
+Released ad hoc product patches
+    </pre>
+          <hr />
+        </div>    
+        
+
+        <div id="bottomright">
+                <h1 >Education:</h1>
+    <pre >- Faculty of Computers and<br/>
+Information, Menoufa University,Attended<br/>
+from 2019 until now<br/>
+Department of Computer Science
+</pre>  <hr />
+            <p>- Udemy online cources for business and programming</p>  <hr />
+            <p >- sololearn (for learning programming languages)</p>  <hr />
+        <p > - coursera online cources for acadmic learning</p>
+    </div>
+        </div>
+
+        <footer>
+        
+        <p>find me in soial media </p>
+          
+        
+        </footer>
+   
+
+
+        </div>  
+
+  
+    
+    );
+    
+    }
+    export default App
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM, {render} from 'react-dom';
+import App from './components/App'
+import './app.css'
+
+ReactDOM.render(<App></App>,
+    document.getElementById('root'));
